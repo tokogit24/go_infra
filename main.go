066f1c8b155fd619b2e5211b59a577e87788de6f
tokogit2021 <rodrigue.tokot@gmail.com>
2024-05-23 19:08:29 -0500

    updated main.go

--- main.go
+++ main.go
@@ -41,7 +41,7 @@ func main() {
 		}
 
 		kp, err := ec2.NewKeyPair(ctx, "local-ssh", &ec2.KeyPairArgs{
-			PublicKey: pulumi.String("ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDevxctOV+DNsK5Yz0MR6n3HYXh8MPvQ7Eq1yb/vkLwmyr/XNMp2IN7/wG2Tt+qY3avLIDzBoKkEBSO9o31OuCjmmNUC9frPlRaJoeSwyN0+5JBlfpkaMRG1HJG0Quaep6eWyCpFufoQCEX5+H6FE2usQB34cGaVmWsP69MrhLkY4Q/JoJYlJnFqCjX7PWgjkpUWkMuH1EeWopL4pLWTRrdutleRBZYrNCVUocqm8KBX4/jm+hm7Vfx5ULLovvNpz4d06U9epFjY0FcqYQfeWPOp5AfJdeUItegyMDL34l8KqzJrJKWtnbAbiel8NN9pYG8DdQw22y2kTMno7p1IxW864bI8mTUI5MYrMNGo6t+yZVa1k162HzBnDHa8UHx/T6kvV4tCrWLpc+hxDkdpGiEFcEHx7Dk7ZXw2Gv/sHUIoxVgShlh7TvEm/fTITxdYcCuz7g/JPNzv+m6InmWNH9tW/P0a7dbPkT478/wObuzuaR0dtXxf9SM/bJYnmxEMFc= kantan@kantans-Virtual-Machine.local"),
+			PublicKey: pulumi.String("ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDkyzYvvsYz/VAzWSDiHgL1vO0DHHkvpHrsbBb2teBrJ51vL5w7tIuMfJLbAsACwNYc65VKCBIo0wfRu2x+D+Uwk2xIXsn6Ex7N43LfNfuJjcBkh4aLILYgGWt9v0bnPMXkxiJwOawTIRGUc2xJvddRl95cy7D0B4P8pnR3iyKesCtxMWvce2xL3rTiuRqFOcAkWK+0ui9wTtxDBsmnealRZsG5OHXkAsqPeUzkd2Clt3Z/cTgcdgvqJoRrubgm5MpGLK6/m0D4h89v6B9MUR5hbZbik2vj1Z/81hnUHQjdHVXbxaINL7d/gA2deyljpIZn+OCmuGn9OTYd/hYVUtjDlAS8xX5O/bqHP3dRk9yTYbWn4BEAn3WmvJTC55PoUShKBwYWl+cmEGt4Yr2woHOq2l9wRDEtY2bVSnGsGHwEgh6fRJzLyndsftQ4jw6SBH0GM9HVEa6TOXFay/Ahpfg4rS8sHGX0+mEvK8xf3GuJbS6Kk8kkw782PRFACbyHmJM= jasmine@DESKTOP-SJH3J0E"),
 		})
 		if err != nil {
 			return err
@@ -50,7 +50,7 @@ func main() {
 		jenkinsServer, err := ec2.NewInstance(ctx, "jenkins-server", &ec2.InstanceArgs{
 			InstanceType:        pulumi.String("t2.micro"),
 			VpcSecurityGroupIds: pulumi.StringArray{sg.ID()},
-			Ami:                 pulumi.String("ami-0ca23709ed2a0fdf9"),
+			Ami:                 pulumi.String("ami-0a0277ba899dd9fd3"),
 			KeyName:             kp.KeyName,
 		})
 
